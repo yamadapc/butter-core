8edeff182fd7d0e8618bb95d87c797a48ba4fa39
yamadapc <tacla.yamada@gmail.com>
2014-10-17 19:47:21 -0400

    Initial commit
    For now, all this includes is a very simple Bencoded values parser
    written using Attoparsec. I'd very much like to see how much this
    beautiful, small implementation compares (in terms of performance) to
    the C parser, or other lower-level implementations in haskell.

---
+++ src/Butter/Core/Bencode.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Butter.Core.Bencode where
+
+import Control.Applicative
+import Data.Attoparsec.Char8 as AP
+import qualified Data.ByteString as B
+import Data.Map as M (Map, fromList)
+
+data BEValue = BEInt Integer
+             | BEString B.ByteString
+             | BEList [BEValue]
+             | BEDict (Map B.ByteString BEValue)
+  deriving(Eq, Ord, Show)
+
+parseBEValue :: Parser BEValue
+parseBEValue = parseBEInt    <|>
+               parseBEString <|>
+               parseBEList   <|>
+               parseBEDict
+
+parseBEInt :: Parser BEValue
+parseBEInt = do
+    _ <- char 'i'
+    i <- decimal
+    _ <- char 'e'
+    return $ BEInt i
+
+parseBEString :: Parser BEValue
+parseBEString = do
+    l <- decimal
+    _ <- char ':'
+    BEString <$> AP.take l
+
+parseBEList :: Parser BEValue
+parseBEList = do
+    _ <- char 'l'
+    bs <- many parseBEValue
+    _ <- char 'e'
+    return $ BEList bs
+
+parseBEDict :: Parser BEValue
+parseBEDict = do
+    _ <- char 'd'
+
+    kvs <- many $ do
+      BEString k <- parseBEString
+      v <- parseBEValue
+      return (k, v)
+    _ <- char 'e'
+
+    return $ BEDict $ M.fromList kvs
---
+++ test/ButterCoreBencodeSpec.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+module ButterCoreBencodeSpec where
+
+import Data.Attoparsec
+import Data.ByteString
+import Data.Map
+import Test.Hspec
+
+import Butter.Core.Bencode
+
+spec :: Spec
+spec =
+    describe "BERead String" $ do
+      describe "parseBEValue" $ do
+        it "parses `BEInt`s" $
+          parseOnly parseBEValue "i123e" `shouldBe` Right (BEInt 123)
+
+        it "parses `BEString`s" $
+          parseOnly parseBEValue "5:hello" `shouldBe` Right (BEString "hello")
+
+        it "parses `BEList`s" $
+          parseOnly parseBEValue "l5:helloi123ee"
+            `shouldBe` Right (BEList [BEString "hello", BEInt 123])
+
+        it "parses `BEDict`s" $ do
+            let Right (BEDict m) = parseOnly parseBEValue "d5:helloi132ee"
+            (m ! ("hello" :: ByteString)) `shouldBe` (BEInt 132)
+
+        it "fails to parse invalid input" $
+          parseOnly parseBEValue "asdfkajsdfaksdjf"
+            `shouldSatisfy` \case Left _ -> True ; _ -> False
