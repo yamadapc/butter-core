a02bda64a79c5e8f47d232ce606127bf7baf3a17
yamadapc <tacla.yamada@gmail.com>
2014-10-27 10:51:01 -0400

    Add initial `Util` module for URL encoding
    This adds a draft of a module (mostly) extracted from `combinatorrent`,
    which exposes a patched version of URL encoding for our announce
    queries.

---
+++ src/Butter/Core/Util.hs
@@ -0,0 +1,37 @@
+-- |
+-- NOTE
+-- This module is to be replaced with a complete better implementation of
+-- an URL encoding library, which is currently being worked on.
+module Butter.Core.Util where
+
+import Data.Char (ord)
+import Data.List (partition)
+import Numeric (showHex)
+
+-- |
+-- URL Encodes a String
+-- (was mostly copied from the combinatorrent copy of the old HTTP library,
+-- I'm working on a separate library which will expose more general and
+-- efficient version of this)
+urlEncode' :: String -> String
+urlEncode' [] = []
+urlEncode' (h:t) = let str = if reserved (ord h) then escape h else [h]
+                  in str ++ urlEncode' t
+ where reserved x | x >= ord 'a' && x <= ord 'z' = False
+                  | x >= ord 'A' && x <= ord 'Z' = False
+                  | x >= ord '0' && x <= ord '9' = False
+                  | x <= 0x20 || x >= 0x7F = True
+                  | otherwise = x `elem` map ord ";/?:@&=+,${}|\\^[]`<>#%\""
+       escape x = '%': showHex (ord x) "" -- This is wrong usage of `showHex`
+
+-- |
+-- Encodes an array of key-value tuples into an URL-encoded querystring
+-- (look at the comment in `urlEncode`)
+urlEncodeVars :: [(String, String)] -> String
+urlEncodeVars ((n,v):t) =
+    let (same,diff) = partition ((==n) . fst) t
+    in urlEncode' n ++ '=' : foldl (\x y -> x ++ ',' : urlEncode' y) (urlEncode' v) (map snd same)
+       ++ urlEncodeRest diff
+       where urlEncodeRest [] = []
+             urlEncodeRest diff = '&' : urlEncodeVars diff
+urlEncodeVars [] = []
