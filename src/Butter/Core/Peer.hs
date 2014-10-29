e47ea7f32369398998ef7348970bb7df0583f3ed
yamadapc <tacla.yamada@gmail.com>
2014-10-29 18:15:40 -0400

    Start drafting PeerWire socket opening
    This starts to draft the basic data models for the peerwire protocol and
    the connection opening logic and interface for it. We need to solve how
    serialization and serialization will work out, really, but I think I'm
    going to test this at the application level (on the currently private
    butter-cli consumer).

--- src/Butter/Core/Peer.hs
+++ src/Butter/Core/Peer.hs
@@ -3,12 +3,22 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Butter.Core.Peer ( Peer(..)
+{-# LANGUAGE RecordWildCards #-}
+module Butter.Core.Peer (
+                        -- ** Peer data types
+                          Peer(..)
                         , PeerId
                         , newPeerId
+                        -- ** Peer Wire Protocol
+                        , PeerWireMessage(..)
+                        , PWBlock
+                        , PWInteger
+                        , connectToPeer
+                        , sendPeerHandshake
+                        -- ** Binary parsing functions
                         , getAll
                         , putAll
-                        -- Exported from `Data.Binary`
+                        -- ** Exported from `Data.Binary`
                         , Binary(..)
                         , encode
                         , decode
@@ -20,24 +30,21 @@ import Data.Binary (Binary, encode, decode, get, put)
 import Data.Binary.Get (Get, isEmpty)
 import Data.Binary.Put (Put)
 import qualified Data.ByteString as B (ByteString)
+import qualified Data.ByteString.Lazy as L (ByteString)
 import qualified Data.ByteString.Char8 as C (pack)
 import Data.Time (formatTime, getCurrentTime)
-import Data.Word (Word16, Word32)
+import Data.Word (Word8, Word16, Word32)
 import GHC.Generics (Generic)
+import Network.Socket
 import System.Locale (defaultTimeLocale)
 import System.Posix.Process (getProcessID) -- sigh...
 
-type PeerId = B.ByteString
-
-data Peer = Peer { pIp   :: Word32
-                 , pPort :: Word16
+data Peer = Peer { pIp   :: HostAddress -- type HostAddress = Word32
+                 , pPort :: Word16      -- newtype PortNumber = PortNum Word16
                  }
   deriving(Generic, Eq, Ord, Show)
 
-instance Binary Peer
-instance Binary [Peer] where
-    get = getAll
-    put = putAll
+type PeerId = B.ByteString
 
 -- |
 -- Generates a new peer id randomly
@@ -46,6 +53,46 @@ newPeerId = do
     t <- reverse <$> formatTime defaultTimeLocale "%s" <$> getCurrentTime
     pid <- getProcessID
     return $ C.pack $ take 20 $ "BU-" ++ show pid ++ t ++ repeat '0'
+
+type PWInteger = Word32
+type PWBlock = L.ByteString
+data PeerWireMessage = PWKeepAlive
+                     | PWChoke
+                     | PWUnchoke
+                     | PWNotInterested
+                     | PWInterested
+                     | PWHave { pwHaveIndex :: PWInteger
+                              }
+                     | PWBitField [Word8]
+                     | PWRequest { pwRequestIndex  :: PWInteger
+                                 , pwRequestBegin  :: PWInteger
+                                 , pwRequestLength :: PWInteger
+                                 }
+                     | PWPiece { pwPieceIndex  :: PWInteger
+                               , pwPieceCancel :: PWInteger
+                               , pwPieceBlock  :: PWBlock
+                               }
+                     -- | PWPort
+
+-- |
+-- Connects to a peer and returns an open socket with it. Assumes we're
+-- inside of 'withSocketsDo'.
+connectToPeer :: Peer -> IO Socket
+connectToPeer Peer{..} = do
+    sock <- socket AF_INET Stream defaultProtocol
+    connect sock sockaddr
+    return sock
+  where sockaddr = SockAddrInet (PortNum pPort) pIp
+
+-- |
+-- Sends the handshake peerwire message through a socket
+sendPeerHandshake :: Socket -> IO ()
+sendPeerHandshake = undefined
+
+instance Binary Peer
+instance Binary [Peer] where
+    get = getAll
+    put = putAll
 
 -- |
 -- Puts a list of `Binary` instance elements directly concatenating their
