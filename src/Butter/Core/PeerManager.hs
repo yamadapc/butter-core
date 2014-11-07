85d1fa8211eaafabc4c10451ccf87cf0046dc9ba
yamadapc <tacla.yamada@gmail.com>
2014-11-06 22:10:25 -0500

    Flush out more ideas about peer manager modeling
    I think I got basic state inputting set-up now. This means, consuming a
    `PeerSource` - whether it's a `TrackerClient` or an open socket and
    merging several `PeerSource`s; as well, as stopping/resuming a
    `PeerSource` on demand.
    
    I haven't tested this, but "hey!" it compiles.
    
    :P

--- src/Butter/Core/PeerManager.hs
+++ src/Butter/Core/PeerManager.hs
@@ -1,80 +1,109 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 module Butter.Core.PeerManager where
 
-import Butter.Core.MetaInfo (InfoHash)
-import Butter.Core.Peer (Peer(..), PeerEvent, startPeer)
-import Butter.Core.PeerWire
-import Control.Concurrent (forkIO, forkFinally)
-import Control.Concurrent.Chan (Chan, newChan, readChan)
-import Control.Concurrent.STM (TVar, atomically, modifyTVar, newTVarIO)
-import Control.Exception (SomeException, try)
-import Control.Monad (forever, void)
-import qualified Data.Map.Strict as Map (Map, empty, insert)
-import Data.Conduit.Network (sourceSocket)
+import Butter.Core.MetaInfo (MetaInfo(..), FileInfo(fiHash))
+import Butter.Core.Peer (Peer(..), PeerMessage(..), createConnection,
+                         receiveConnection)
+import Butter.Core.PeerWire (PeerId, PWInteger)
+import Butter.Core.Tracker.Client (ClientOptions(..), TrackerClient(..),
+                                   readPeerAddr)
+import Control.Applicative ((<$>), (<*>))
+import Control.Concurrent (MVar, forkIO, newMVar, tryPutMVar, tryTakeMVar,
+                           readMVar)
+import Control.Concurrent.STM -- (TChan, TVar, atomically, modifyTVar, newTChanIO,
+                               -- newTVarIO, readTVar, writeTChan)
+import Control.Monad (void, when)
+import qualified Data.Map.Strict as Map (Map, empty, insert, size)
 import Network (PortID(..), listenOn)
-import Network.Socket (Socket, SockAddr(..), PortNumber(..), accept, close)
+import Network.Socket (PortNumber(..), accept)
 
-type PeerHandler = Socket -> SockAddr -> IO ()
+data PeerManager =
+    PeerManager { pmPeerId   :: PeerId
+                , pmPort     :: PortNumber
+                , pmMetaInfo :: MetaInfo
 
-data PeerManager = PeerManager { pmPeerId         :: PeerId
-                               , pmPort           :: PortNumber
-                               , pmInfoHash       :: InfoHash
-                               , pmMapVar         :: TVar (Map.Map PeerId Peer)
-                               , pmPeerEventsChan :: Chan PeerEvent
-                               }
+                , pmSource :: PeerSource
 
-newPeerManager :: PeerId -> PortNumber -> InfoHash -> IO PeerManager
-newPeerManager peerId port infoHash = do
-    sock            <- listenOn $ Network.PortNumber port
-    eventsChan      <- newChan
-    pmapVar         <- newTVarIO Map.empty
+                , pmConnections :: TVar (Map.Map PeerId Peer)
+                , pmPieces      :: TVar (Map.Map PWInteger [PeerId])
+                , pmMsgsChan    :: TChan PeerMessage
+                }
 
-    let m = PeerManager peerId port infoHash pmapVar eventsChan
+newPeerManager :: PeerId -> PortNumber -> MetaInfo -> PeerSource
+               -> IO PeerManager
+newPeerManager peerId port mi s =
+    PeerManager peerId port mi s <$> newTVarIO Map.empty
+                                 <*> newTVarIO Map.empty
+                                 <*> newTChanIO
 
-    _ <- forkIO $ handleEvents eventsChan pmapVar
-    _ <- forkIO $ acceptPeers sock (handlePeer m)
+addPeer :: PeerManager -> Peer -> IO ()
+addPeer m peer = do
+    sz <- atomically $
+        modifyTVar (pmConnections m) (Map.insert (pId peer) peer) >>
+        Map.size <$> readTVar (pmConnections m)
 
-    return m
+    when (sz > 50) $ stopPeerSource (pmSource m)
 
-acceptPeers :: Socket -> PeerHandler -> IO ()
-acceptPeers sock handler = forever $ do
-    (peersock, addr) <- accept sock
-    forkFinally (handler peersock addr) (\_ -> close sock)
+-- ** Acceptor and Connector types
 
-handleEvents :: Chan PeerEvent -> TVar (Map.Map PeerId Peer) -> IO ()
-handleEvents ch pmapVar = consumeChannel ch (handleEvent pmapVar)
+runAcceptor :: PeerId -> PortNumber -> MetaInfo -> IO PeerSource
+runAcceptor peerId port mi = do
+    m <- newMVar ()
+    c <- newTChanIO
+    sock <- listenOn $ PortNumber port
+    let self = (m, c)
 
-handleEvent :: TVar (Map.Map PeerId Peer) -> PeerEvent -> IO ()
-handleEvent = undefined
+    void $ forkIO $ loop self sock
 
-addFromChan :: PeerManager -> Chan PeerAddr -> IO ()
-addFromChan m pchan = forever $ do
-    peerAddr <- readChan pchan
-    esock <- try $ connectToPeer peerAddr :: IO (Either SomeException Socket)
-    case esock of
-        Left _ -> return ()
-        Right sock -> do
-            sendMessage sock PWHandshake { pwHandshakePeerId = pmPeerId m
-                                         , pwHandshakeInfoHash = pmInfoHash m
-                                         }
-            peer <- startPeer (pmPeerEventsChan m) sock Nothing
-            atomically $ modifyTVar (pmMapVar m) (Map.insert (pId peer) peer)
+    return self
+  where infoHash = fiHash $ miInfo mi
+        loop self@(_, c) sock = guardRunning self $ do
+            (peersock, _) <- accept sock
+            void $ forkIO $ receiveConnection peerId infoHash peersock >>=
+                            atomically . writeTChan c
+            loop self sock
 
-handlePeer :: PeerManager -> Socket -> SockAddr -> IO ()
-handlePeer m sock _ = do
-    info@(_, PWHandshake{..}) <- receiveHandshake $ sourceSocket sock
-    if pwHandshakeInfoHash == pmInfoHash m
-        then do
-            sendMessage sock PWHandshake { pwHandshakePeerId = pmPeerId m
-                                         , pwHandshakeInfoHash = pmInfoHash m
-                                         }
-            peer <- startPeer (pmPeerEventsChan m) sock (Just info)
-            atomically $ modifyTVar (pmMapVar m) (Map.insert (pId peer) peer)
-        else void $ close sock
+runConnector :: TrackerClient -> IO PeerSource
+runConnector tc = do
+    m <- newMVar ()
+    c <- newTChanIO
+    let self = (m, c)
 
-consumeChannel :: Chan a -> (a -> IO b) -> IO ()
-consumeChannel c f = void $ forkIO $ forever $ do
-    x <- readChan c
-    _ <- forkIO $ void $ f x
-    return ()
+    void $ forkIO $ loop self
+
+    return self
+  where opts = tcOptions tc
+        peerId = cPeerId opts
+        infoHash = cInfoHash opts
+        loop self@(_, c) = guardRunning self $ do
+            peerAddr <- readPeerAddr tc
+            void $ forkIO $ createConnection peerId infoHash peerAddr >>=
+                            atomically . writeTChan c
+            loop self
+
+-- *** Stopping mechanism
+
+type PeerSource = (MVar (), TChan Peer)
+
+mergePeerSources :: PeerSource -> PeerSource -> IO PeerSource
+mergePeerSources (_, c1) (_, c2) = do
+    m <- newMVar ()
+    c <- mergeTChans c1 c2
+    return (m, c)
+
+resumePeerSource :: PeerSource -> IO ()
+resumePeerSource (lock, _) = void $ tryPutMVar lock ()
+
+stopPeerSource :: PeerSource -> IO ()
+stopPeerSource (lock, _) = void $ tryTakeMVar lock
+
+guardRunning :: PeerSource -> IO a -> IO a
+guardRunning (lock, _) f = readMVar lock >> f
+
+mergeTChans :: TChan a -> TChan a -> IO (TChan a)
+mergeTChans c1 c2 = do
+  c <- newTChanIO
+  void $ forkIO $ atomically $ readTChan c1 `orElse` readTChan c2 >>=
+                               writeTChan c
+  return c
